769f303b06d98a05afc4b10548db68f5ee328c09
Conner Alegre <cga0@protonmail.com>
2016-07-28 02:14:35 -0600

    Heroku

--- src/api/dataApi.js
+++ src/api/dataApi.js
@@ -1,24 +1,24 @@
 class DataApi {
     static getInitialData() {
-       return fetch('//localhost:5000/mostComplaints', {
+       return fetch('//sheltered-stream-12729.herokuapp.com:5000/mostComplaints', {
            headers: new Headers({'X-Custom-Header': 'AK'})
        });
     }
 
     static getStateComplaintData(state) {
-        return fetch('//localhost:5000/mostComplaints', {
+        return fetch('//sheltered-stream-12729.herokuapp.com:5000/mostComplaints', {
            headers: new Headers({'X-Custom-Header': state})
        });
     }
 
     static getComplaintData(complaint) {
-        return fetch('//localhost:5000/fastestGrowing', {
+        return fetch('//sheltered-stream-12729.herokuapp.com/fastestGrowing', {
            headers: new Headers({'X-Custom-Header': complaint})
        });
     }
 
     static getBirthData(company) {
-        return fetch('//localhost:5000/births', {
+        return fetch('//sheltered-stream-12729.herokuapp.com:5000/births', {
            headers: new Headers({'X-Custom-Header': company})
        });
     }
